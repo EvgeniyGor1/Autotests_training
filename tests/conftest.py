4885078a1631d780d57f0b81c99e13c9b6b44ba7
Evgeniy <gorlow.jenya@gmail.com>
2024-09-04 15:33:39 +0300

    set window size for webdriver

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ DEBUG_MODE = 0
 @pytest.fixture(scope='module')
 def browser():
     options = webdriver.ChromeOptions()
+    options.add_argument('--window-size=1920,1080')
     if not DEBUG_MODE:
         options.add_argument('--headless')
     else:
