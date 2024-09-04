fcc00aa363495e9d4aee81813e3c6acb63ece7ba
Evgeniy <gorlow.jenya@gmail.com>
2024-09-04 14:07:37 +0300

    some changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,17 +4,19 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
 
-DEBUG_MODE = 0
+DEBUG_MODE = 1
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def browser():
     options = webdriver.ChromeOptions()
     if not DEBUG_MODE:
         options.add_argument('--headless')
+        options.add_argument('--log-level=3')
     else:
         options.add_argument('--disable-search-engine-choice-screen')
 
-    with webdriver.Chrome(options=options) as browser:
-        browser.implicitly_wait(5)
-        yield browser
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(5)
+    yield browser
+    browser.close()
