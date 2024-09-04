ae64316081d17c321cd7dd4cc19bfba5b869bb6e
Evgeniy <gorlow.jenya@gmail.com>
2024-09-04 15:35:49 +0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,11 +7,10 @@ DEBUG_MODE = 0
 @pytest.fixture(scope='module')
 def browser():
     options = webdriver.ChromeOptions()
+    options.add_argument('--disable-search-engine-choice-screen')
     options.add_argument('--window-size=1920,1080')
     if not DEBUG_MODE:
         options.add_argument('--headless')
-    else:
-        options.add_argument('--disable-search-engine-choice-screen')
 
     with webdriver.Chrome(options=options) as browser:
         browser.implicitly_wait(5)
