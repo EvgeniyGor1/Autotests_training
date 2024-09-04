6b361af509e1cfe77c62eeccae701328f665eee6
Evgeniy <gorlow.jenya@gmail.com>
2024-09-04 14:23:24 +0300

    Revert "some changes"
    This reverts commit fcc00aa363495e9d4aee81813e3c6acb63ece7ba.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,16 +7,14 @@ import pytest
 DEBUG_MODE = 0
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope='module')
 def browser():
     options = webdriver.ChromeOptions()
     if not DEBUG_MODE:
         options.add_argument('--headless')
-        options.add_argument('--log-level=3')
     else:
         options.add_argument('--disable-search-engine-choice-screen')
 
-    browser = webdriver.Chrome(options=options)
-    browser.implicitly_wait(5)
-    yield browser
-    browser.close()
+    with webdriver.Chrome(options=options) as browser:
+        browser.implicitly_wait(5)
+        yield browser
--- tests/test_buttons.py
+++ tests/test_buttons.py
@@ -13,7 +13,6 @@ def test_buttons(browser):
 
 @pytest.mark.buttons
 def test_simple_button(browser):
-    browser.get('https://www.qa-practice.com/elements/button/simple')
     click_button = browser.find_element(By.ID, 'submit-id-submit')
     click_button.click()
     assert 'Submitted' in browser.find_element(By.ID, 'result-text').text
@@ -21,20 +20,18 @@ def test_simple_button(browser):
 
 @pytest.mark.buttons
 def test_looks_like_a_button(browser):
-    browser.get('https://www.qa-practice.com/elements/button/like_a_button')
-    # all_inner_buttons = browser.find_element(By.CLASS_NAME, 'tabs').find_elements(By.TAG_NAME, 'li')
-    # looks_like_a_button = all_inner_buttons[1]
-    # looks_like_a_button.click()
+    all_inner_buttons = browser.find_element(By.CLASS_NAME, 'tabs').find_elements(By.TAG_NAME, 'li')
+    looks_like_a_button = all_inner_buttons[1]
+    looks_like_a_button.click()
     browser.find_element(By.CLASS_NAME, 'a-button').click()
     assert 'Submitted' in browser.find_element(By.ID, 'result-text').text
 
 
 @pytest.mark.buttons
 def test_disabled_button(browser):
-    browser.get('https://www.qa-practice.com/elements/button/disabled')
-    # all_inner_buttons = browser.find_element(By.CLASS_NAME, 'tabs').find_elements(By.TAG_NAME, 'li')
-    # disabled_button = all_inner_buttons[2]
-    # disabled_button.click()
+    all_inner_buttons = browser.find_element(By.CLASS_NAME, 'tabs').find_elements(By.TAG_NAME, 'li')
+    disabled_button = all_inner_buttons[2]
+    disabled_button.click()
     state = Select(browser.find_element(By.ID, 'id_select_state'))
     state.select_by_value('enabled')
     browser.find_element(By.ID, 'submit-id-submit').click()
